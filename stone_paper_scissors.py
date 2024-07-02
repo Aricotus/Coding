6b288ec8c08d6b78673317914b7c5528caecae6d
Aricotus <shenoy.vineeth19@gmail.com>
2024-07-02 11:31:09 +0530

    Update stone_paper_scissors.py

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -1,13 +1,14 @@
 import random
 import win32com.client
+import streamlit as st
 speaker=win32com.client.Dispatch("SAPI.SpVoice")
 
 d=0
 b=0
 while(True):
- print("Enter 0 to exit")
- print("Enter 1 to choose stone\nEnter 2 to choose paper\nEnter 3 to choose scissors")
- a=int(input("Enter choice"))
+ st.write("Enter 0 to exit")
+ st.write("Enter 1 to choose stone\nEnter 2 to choose paper\nEnter 3 to choose scissors")
+ a=int(st.text_input("Enter choice"))
  if(a>3 or a<0):
      raise ValueError("Invalid Input")
  else:
@@ -17,41 +18,41 @@ while(True):
   match a:
     case 1:
         if(c==1):
-            print("Both choose stone.Tied")
+            st.write("Both choose stone.Tied")
             speaker.Speak("Both choose stone Tied")
         elif(c==2):
-            print("Computer choose paper.You lost")
+            st.write("Computer choose paper.You lost")
             speaker.Speak("Computer choose paper You lost")
         elif(c==3):
             d+=1
-            print("Computer choose scissors.You Won")
+            st.write("Computer choose scissors.You Won")
             speaker.Speak("Computer choose scissors You Won")
     case 2:
         if(c==1):
             d+=1
-            print("Computer choose stone.You Won")
+            st.write("Computer choose stone.You Won")
             speaker.Speak("Computer choose stone You Won")
         elif(c==2):
-            print("Both choose Paper.Tied")
+            st.write("Both choose Paper.Tied")
             speaker.Speak("Both choose Paper Tied")
         elif(c==3):
-            print("Computer choose scissors.You Lost")
+            st.write("Computer choose scissors.You Lost")
             speaker.Speak("Computer choose scissors You lost")
     case 3:
         if(c==1):
-            print("Computer choose stone.You Lost")
+            st.write("Computer choose stone.You Lost")
             speaker.Speak("Computer choose stone You lost")
         elif(c==2):
             d+=1
-            print("Computer choose paper.You Won")
+            st.write("Computer choose paper.You Won")
             speaker.Speak("Computer choose paper You Won")
         elif(c==3):
-            print("Both choose Scissors.Tied")  
+            st.write("Both choose Scissors.Tied")  
             speaker.Speak("Both choose Scissors Tied")
  b+=1                  
  if(a==0):
      break     
-print(f"You have played {b} times") 
+st.write(f"You have played {b} times") 
 speaker.Speak(f"You have played {b} times")     
-print(f"You have won {d} times")
+st.write(f"You have won {d} times")
 speaker.Speak(f"You have won {d} times")
